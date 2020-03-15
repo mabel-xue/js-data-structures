8f44b7f31c52aa35e6d8bfee59716b1efc33bc64
Xue Yumei <xueyumei106@gmail.com>
2020-03-15 11:07:28 +0800

    update linkList

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,7 @@
-// 链表节点
+/**
+ * 单向链表节点
+ * 包含两个值: 当前节点的值和一个指向下一个节点的链接
+ */
 class Node {
   constructor(element) {
     this.element = element;
@@ -6,16 +9,16 @@ class Node {
   }
 }
 
-// 链表
+/** 单向链表 */
 class LinkedList {
   constructor() {
     this.head = null;
   }
 
-  // 在列表末尾添加一个元素
-  add(element) {
+  /** 在列表末尾添加一个元素 */
+  push(element) {
     const node = new Node(element);
-    let current;
+    let current; // 指针
     if (this.head === null) {
       this.head = node;
     } else {
@@ -27,40 +30,38 @@ class LinkedList {
     }
   }
 
-  // 在列表头部添加一个元素
-  prepend(element) {
+  /** 在列表头部添加一个元素 */
+  unshift(element) {
     const node = new Node(element);
-    let n;
     if (this.head === null) {
       this.head = node;
     } else {
-      n = this.head;
+      node.next = this.head;
       this.head = node;
-      this.head.next = n;
     }
   }
 
-  // 搜索
-  contains(element) {
+  /** 搜索元素是否存在于链表中 */
+  search(element) {
     let current = this.head;
     while (current !== null && current.element !== element) {
       current = current.next;
     }
-    return current === null ? false : true;
+    return current !== null;
   }
 
-  // 删除
+  /** 删除 */
   remove(element) {
     if (this.head === null) {
       return false;
     }
     let current = this.head;
-    if (current.element === element) {
+    if (current.element === element) { // 删除元素在头结点中
       this.head.next ? this.head = this.head.next : this.head = null;
       return true;
     }
     
-    let prev;
+    let prev; // 指针的上一节点
     while (current !== null && current.element !== element) {
       prev = current;
       current = current.next;
@@ -72,7 +73,7 @@ class LinkedList {
     return false;
   }
 
-  // 遍历
+  /** 遍历 */ 
   * traverse() {
     let current = this.head;
     while(current !== null) {
@@ -81,8 +82,9 @@ class LinkedList {
     }
   }
 
-  // 反向遍历
+  /** 反向遍历 */
   * reverse() {
+    // step1: 调序重排
     let current = this.head;
     let prev = null;
     let next;
@@ -93,7 +95,7 @@ class LinkedList {
       current = next;
     }
     this.head = prev;
-    // 遍历
+    // step2: 正序遍历
     current = this.head;
     while(current !== null) {
       yield current.element;
@@ -101,3 +103,20 @@ class LinkedList {
     }
   }
 }
+
+// 范例
+const list = new LinkedList();
+
+list.push('1');
+list.push('2');
+list.unshift('-1');
+list.unshift('0');
+list.remove('-1')
+
+const traverse = list.traverse();
+console.log(traverse.next());
+console.log(traverse.next());
+
+const reverse = list.reverse();
+console.log(reverse.next());
+console.log(reverse.next());
