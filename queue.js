14adb89532de58a9dad2eebbfb0d5829acaadd95
Xue Yumei <xueyumei106@gmail.com>
2020-03-17 13:51:58 +0800

    update quene

--- queue.js
+++ queue.js
@@ -1,34 +1,52 @@
-// 队列节点
+/** 队列节点 */
 class Node {
   constructor(element) {
     this.element = element;
     this.next = null;
   }
 }
-// 队列
+
+/** 队列 */
 class Queue {
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  // 入队
+  /** 
+   * 入队
+   * 只能从队尾添加元素
+   */
   enqueue(element) {
     const node = new Node(element);
-    if (this.tail !== null) {
+    if (this.tail) {
       this.tail.next = node;
     }
     this.tail = node;
-    if (this.head === null) {
+    if (!this.head) {
       this.head = node;
     }
   }
 
-  // 出队
+  /** 
+   * 出队
+   * 只能从头部删除元素
+   */ 
   dequeue() {
     this.head = this.head.next;
-    if (this.head === null) {
+    if (!this.head) {
       this.tail = null;
     }
   }
 }
+
+// 示例
+const quene = new Queue();
+
+quene.enqueue(1);
+quene.enqueue(2);
+quene.enqueue(3);
+
+quene.dequeue();
+
+console.log(quene);
