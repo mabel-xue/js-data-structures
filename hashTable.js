8a0d0084058607cfd26ecf2b39db3ac2001a9eae
Xue Yumei <xueyumei106@gmail.com>
2020-03-20 09:08:45 +0800

    update hashTable

--- hashTable.js
+++ hashTable.js
@@ -1,10 +1,10 @@
-// 哈希表 (不处理冲突)
-class HashTable {
+/** (不处理冲突的)哈希表 */ 
+class HashTable_WithoutSolveClash {
   constructor(size) {
     this.table = new Array(size);
   }
 
-  // 哈希函数 - 除留余数法
+  /** 哈希函数 - 除留余数法 */
   hash(key) {
     let total = 0;
     for (let i = 0; i < key.length; i++) {
@@ -36,13 +36,25 @@ class HashTable {
   }
 }
 
-// 哈希表 （处理冲突）
-class HashTable {
+// 示例
+const h1 = new HashTable_WithoutSolveClash(1993);
+h1.set('X', 'xwz');
+h1.set('H', 'hjy');
+h1.set('X', 'xxs'); // 和第一条数据冲突
+
+console.log(h1.get('X')); // xxs (冲突数据覆盖了之前的数据)
+
+h1.delete('X');
+
+h1.show();
+
+/** (处理冲突的)哈希表 */
+class HashTable_WithSolveClash {
   constructor(size) {
     this.table = new Array(size);
   }
 
-  // 哈希函数 - 平方求和法
+  /** 哈希函数 - 平方求和法 */
   hash(key) {
     let total = 0;
     for (let i = 0; i < key.length; i++) {
@@ -52,7 +64,7 @@ class HashTable {
     return s.charAt(s.length / 2 - 1) * 10 + s.charAt(s.length / 2) * 1;
   }
 
-  // 解决哈希冲突 - 线性开放定址法
+  /** 解决哈希冲突 - 线性开放定址法 */
   solveClash(index, value) {
     const table = this.table;
     for (let i = 0; index + i < table.length; i++) {
@@ -80,3 +92,12 @@ class HashTable {
     }
   }
 }
+
+// 示例
+const h2 = new HashTable_WithSolveClash(1993);
+
+h2.set('X', 'xwz');
+h2.set('H', 'hjy');
+h2.set('X', 'xxs');
+
+h2.show(); // 原先被覆盖的值正常显示了
